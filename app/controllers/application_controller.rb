35d793a04c218fb967b1c525c8cfea9acecac724
mewhu <mew31716@gmail.com>
2018-07-15 20:39:37 +0800

    1) apply "devise" authentication function to controller "admin::restaurant" too. | 2) define function "admin?" in the user model class created from "devise". It could be called by any controller which needs judging the authentication level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+  def authenticate_admin
+    if !(current_user.admin?)
+      flash[:alert] = "Authentication Level : Normal"
+      redirect_to root_path
+    end
+  end
 end
