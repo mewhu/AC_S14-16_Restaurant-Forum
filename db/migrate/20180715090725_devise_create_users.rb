c809c05da6070741685e262589cf3c13c9645289
mewhu <mew31716@gmail.com>
2018-07-15 17:55:28 +0800

    create user model via "devise"

--- db/migrate/20180715090725_devise_create_users.rb
+++ db/migrate/20180715090725_devise_create_users.rb
@@ -32,6 +32,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## add following attributes for "Restaurant Forum"
+      t.string :role
 
       t.timestamps null: false
     end
