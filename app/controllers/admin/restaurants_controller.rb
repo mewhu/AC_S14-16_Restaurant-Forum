481774eb3c4cac3b872c07a0a5b52f7f150f811e
mewhu <mew31716@gmail.com>
2018-07-17 23:52:35 +0800

    complete "admin/restaurants#show"

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -10,6 +10,10 @@ class Admin::RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
     @restaurant = Restaurant.new
   end
