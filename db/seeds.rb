72437d83a8a91e25d1f7f092c0425c075c4df85a
mewhu <mew31716@gmail.com>
2018-07-30 08:22:24 +0800

    edit "db/seeds.rb" -> then "rails db:seed" to import default seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# Create seeds for model "Category"
+Category.destroy_all
+
+category_list = [
+  { :name => "中式料理" }, 
+  { :name => "日本料理" }, 
+  { :name => "美式料理" }, 
+  { :name => "泰式料理" }, 
+  { :name => "義式料理" }, 
+  { :name => "法式料理" }, 
+  { :name => "德式料理" }, 
+  { :name => "素食料理" }, 
+]
+
+category_list.each do |category|
+  Category.create(name: category[:name])
+end
+puts "Categories had been created!"
+
+# Default admin
+User.create(email: "admin@user.com", password: "aaaaaa", role: "admin")
+puts "Default admin created!"
