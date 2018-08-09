7a45a29ee48e52791ffe6244c01e5826fa7990bc
mewhu <mew31716@gmail.com>
2018-08-10 05:37:42 +0800

    refactor controller codes : move the devise authentication process to "application_controller.rb"

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :authenticate_admin
   before_action :target_category, :only => [:update, :destroy]
 
